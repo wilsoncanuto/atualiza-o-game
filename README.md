3011e256afb9789fd02b5a7e3aa55e3eb880e8ae
wilson <99556299+wilsoncanuto@users.noreply.github.com>
2022-08-20 14:04:48 -0300

    commit site atualização game
    Atualização game-Stranger Things

---
+++ README.md
@@ -0,0 +1,6 @@
+# Game_Memoria_StrangerThings #
+Memory Game Stranger Things with HTML - CSS - JAVASCRIPT
+
+
+Projeto Game - Stranger Things - 2022
+
---
+++ scripts.js
@@ -0,0 +1,80 @@
+const cards = document.querySelectorAll(".card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+let points = 0;
+let turns = 0;
+
+function updatePoints() {
+  currentPoints = document.getElementById('points');
+  points++;
+  currentPoints.innerHTML = points;
+  }
+
+  function updateTurns() {
+    currentTurns = document.getElementById('turns');
+    turns++;
+    currentTurns.innerHTML = turns;
+    }
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    updatePoints();
+    return;
+}
+
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  musica3.play();
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    musica2.play();
+    resetBoard();
+    
+  }, 1500);
+  updateTurns();
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+})();
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
